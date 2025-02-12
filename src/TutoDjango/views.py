aafca2e0c311be96c529b296bc0b85d61c3871b4
Sh3tys <titouanparzival@gmail.com>
2025-02-12 19:03:00 +0100

    Test repo

--- src/TutoDjango/views.py
+++ src/TutoDjango/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render # type: ignore
 
 
 def index(request):
-    date = datetime.today()
-    return render(request, "TutoDjango/index.html", context={"date" : date})
+    dateActuelle = datetime.today()
+    return render(request, "TutoDjango/index.html", context={"date" : dateActuelle})
